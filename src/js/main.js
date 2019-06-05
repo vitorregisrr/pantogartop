2d9b26d23c0e6b73e1049ec35442adad751577c4
vitorregisr <vitorregisrr@gmail.com>
2019-06-05 10:53:35 -0300

    Improve texts, Improve backgrounds, Add scroll down, Add scroll to articles

--- src/js/main.js
+++ src/js/main.js
@@ -7,14 +7,30 @@
     });
 
     $('#lp-pant--btn__detalhes').click(function () {
+        $('html, body').animate({
+            scrollTop: $("#lp-pant--info__conhecendo").offset().top
+        }, 800);
+
         $('#lp-pant--info__conhecendo').fadeOut(200, function () {
             $('#lp-pant--info__detalhes').fadeIn(200)
         })
     })
 
     $('#lp-pant--btn__conhecendo').click(function () {
+
+        $('html, body').animate({
+            scrollTop: $("#lp-pant--info__detalhes").offset().top
+        }, 400);
+
         $('#lp-pant--info__detalhes').fadeOut(200, function () {
-            $('#lp-pant--info__conhecendo').fadeIn(200)
+            $('#lp-pant--info__conhecendo').fadeIn(200);
         })
+    })
+
+    $('#lp-pant--scrolldown').click(function () {
+
+        $('html, body').animate({
+            scrollTop: $("#lp-pant--info").offset().top
+        }, 600);
     })
 })();
